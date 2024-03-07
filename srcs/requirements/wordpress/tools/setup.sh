681251cc50e816406e33d5a9446010a27372e137
tomatte <daniel.brandao2000@gmail.com>
2024-03-07 17:05:19 -0300

    create a second user

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -9,6 +9,7 @@ if [[ ! -f /var/www/wordpress/wp-config.php ]]; then
     echo running wordpress configuration
     wp config create --dbname=$DB_NAME --dbuser=root --dbhost=$DB_HOST --dbpass=$DB_ROOT_PASSWORD --allow-root
     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+    wp user create $WP_USER_LOGIN $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --display_name=$WP_USER_NAME --allow-root
     wp theme install inspiro --activate --allow-root
     wp plugin update --all --allow-root
     echo FINISHED WP OPERATIONS
